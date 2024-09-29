0187dd85b0fb0a26c6c386514b680f0baa331a1f
asseternity <nakupovasset@gmail.com>
2024-09-29 20:40:11 +0500

    init: project init, install dependencies

---
+++ app.js
@@ -0,0 +1,21 @@
+// dependencies
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const path = require('node:path'); 
+const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// settings
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// launch
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}!`)
+});
